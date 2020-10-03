449cc093ac9954db1de4982ecef331c90f40685d
Chenxuan Zhao <chenxuan.zhao@icloud.com>
2020-10-03 13:43:48 +0800

    fix warnings.

--- src/qboot_aes.c
+++ src/qboot_aes.c
@@ -11,6 +11,7 @@
 #ifdef QBOOT_USING_AES
 
 #include <tinycrypt.h>
+#include <string.h>
 
 static u8 qbt_aes_iv[16];
 static tiny_aes_context qbt_aes_ctx;
@@ -24,12 +25,12 @@ void qbt_aes_decrypt_init(void)
     }
     memset(qbt_aes_iv, 0, sizeof(qbt_aes_iv));
     memcpy(qbt_aes_iv, QBOOT_AES_IV, len);
-    tiny_aes_setkey_dec(&qbt_aes_ctx, QBOOT_AES_KEY, 256);
+    tiny_aes_setkey_dec(&qbt_aes_ctx, (u8 *)QBOOT_AES_KEY, 256);
 }
 
 void qbt_aes_decrypt(u8 *dst_buf, const u8 *src_buf, u32 len)
 {
-    tiny_aes_crypt_cbc(&qbt_aes_ctx, AES_DECRYPT, len, qbt_aes_iv, src_buf, dst_buf);    
+    tiny_aes_crypt_cbc(&qbt_aes_ctx, AES_DECRYPT, len, qbt_aes_iv, (u8 *)src_buf, dst_buf);    
 }
 
 #endif
